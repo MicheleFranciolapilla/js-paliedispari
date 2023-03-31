01435fbb19b9b63aa4856c20e98848dbdb82cdc3
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-03-31 12:58:50 +0900

    Attivazione sezioni: ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,3 +10,49 @@
 // Consigli del giorno
 // 1. Scriviamo sempre in italiano i passaggi che vogliamo fare
 // 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+
+const       disable_btn     = true;
+const       enable_btn      = false;
+
+
+function toggle_on_off(where, btn_status)
+{
+    let     elements    = document.querySelectorAll(where + " .toggle_btn");
+    for (let i = 0; i < elements.length; i++)
+    {
+        elements[i].disabled = btn_status;
+    }
+}
+
+function toggle_none(id_what)
+{
+    let element = document.getElementById(id_what);
+    element.classList.toggle("d-none");
+}
+
+function play_palindrome()
+{
+
+}
+
+function play_even_odd()
+{
+    
+}
+
+function go_to_game(game)
+{
+    toggle_on_off("#btn_nav", disable_btn);   
+    toggle_none("games_box");
+    switch (game)
+    {
+        case 1:
+            toggle_none("palindrome_game");
+            play_palindrome();
+            break;
+        case 2:
+            toggle_none("even_odd_game");
+            play_even_odd();
+            break;
+    }
+}
